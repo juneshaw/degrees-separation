60ed18e8b479e6c0e40eaae455f2deb9bfd7ae8e
June Shaw <june.shaw@me.com>
2015-12-17 00:47:36 -0700

    Images added

--- js/dom.js
+++ js/dom.js
@@ -1,4 +1,5 @@
-//API keys
+//APIs
+
 //TMDB API
 var tmdbKey = "d5a7fb8a0b7dadfb9db32196dd24e784";
 var tmdbUrlKey = "?api_key=" + tmdbKey;
@@ -6,35 +7,23 @@ var tmdbUrlBase = "https://api.themoviedb.org/3/";
 var tmdbUrlConfig = tmdbUrlBase + "configuration" + tmdbUrlKey;
 var tmdbUrlSearchPerson = tmdbUrlBase + "search/person/" + tmdbUrlKey + "&query=";
 var tmdbUrlSearchMovie = tmdbUrlBase + "search/movie/" + tmdbUrlKey + "&query=";
-
-var tmdbUrlImgPerson = tmdbUrlBase + "person/"; // then <id>/images, then access profiles[0].file_path.
-var tmdbUrlImgMovie = tmdbUrlBase + "movie/"; // then <id>/images, then access backdrops[0].file_path
-
+var tmdbUrlImgPerson = tmdbUrlBase;
+var tmdbUrlImgMovie = tmdbUrlBase;
 var tmdbImgSizePerson = "w92";  // hardcoded preferred size
 var tmdbImgSizeMovie = "w185"; // hardcoded preferred size
 var tmdbImgUrlBase = "";
 var tmdbImgUrlPerson = "";
 var tmdbImgUrlMovie = "";
 
-
-
-//
- //"https://api.themoviedb.org/3/search/person/?api_key="+tmdbKey+"&query="
  //Bacon Oracle API
 var baconKey = "38b99ce9ec87";
 var baconUrlKey = "?p=" + baconKey;
 var baconUrlBase = "http://oracleofbacon.org/cgi-bin/json";
 var baconUrlConnect = baconUrlBase + baconUrlKey;
 
-// var apiBaconOracleKey = "?p=38b99ce9ec87";
-// var baconUrlBase =  "http://oracleofbacon.org/cgi-bin/json" + apiBaconOracleKey;
-// var centerParam = "&center=Kevin+Bacon" //Use actor name here
-// var actorsParam = "&a=Kevin+Bacon&b=Kyra+Sedgwick";
-// aha = api call search person
-  // get results.id
-  // do another api search for the image  with that person id.
-// get the configuration (start of program) with
-// https://api.themoviedb.org/3/configuration?api_key=d5a7fb8a0b7dadfb9db32196dd24e784
+//=======================================================
+// When user clicks the submit button for connection,
+// call the connection API
 $('#calcDegSepBtn').click(function() {
 
   //Replace blanks with + (check all blanks)
@@ -46,30 +35,10 @@ $('#calcDegSepBtn').click(function() {
   // for this connection.
   apiGet(url, baconOracleDone, baconOracleFail);
 });
+//=======================================================
 
-//
-//
-//     // /=================
-// function LoadCategories(argument){
-//   var deferred = $.ajax({
-//      // ajax setup
-//   }).then(function(response){
-//      // optional callback to handle this response
-//   });
-//   return deferred;
-// }
-// Then to call LoadContact() after all three ajax calls have returned and optionally executed their own individual callbacks:
-//
-// // setting variables to emphasize that the functions must return deferred objects
-// var deferred1 = LoadCategories($('#Category'));
-// var deferred2 = LoadPositions($('#Position'));
-// var deferred3 = LoadDepartments($('#Department'));
-//
-// $.when(deferred1, deferred2, deferred3).then(LoadContact);
-//
-// /=================
-
-
+//=======================================================
+// Call an API with the given callbacks
 function apiGet(urlToGet, doneFcn, failFcn, doneArray) {
 console.log("url to get: ", urlToGet);
   var getter = $.ajax({
@@ -86,7 +55,10 @@ console.log("url to get: ", urlToGet);
     failFcn(response);
   });
 }
+//=======================================================
 
+//=======================================================
+// Call an API with no callbacks: promises use these.
 function apiGetWait(urlToGet) {
   console.log("urlToGet in apiGetWait:", urlToGet);
   var getter = $.ajax({
@@ -94,12 +66,23 @@ function apiGetWait(urlToGet) {
     method: "GET",
     datatype: "json"}
   );
+  getter.fail(function() {console.log('error in apiGetWait')})
   console.log("apiGetWait getter = ", getter);
   return(getter);
 }
+//=======================================================
 
+//=======================================================
+// When the Bacon Oracle is complete, display the results.
 function baconOracleDone(response) {
-  if (response.status === "success"){
+  if (response.status === "spellcheck") {
+    var length = response.length;
+    response.splice(1, length-1);
+  } else if (response.status === "error") {
+      clearResults();
+      console.log("Error with Bacon Oracle get")
+
+  } else if (response.status === "success") {
 
     // Clear last results
     clearResults();
@@ -112,8 +95,10 @@ function baconOracleDone(response) {
 
     //Display the connection chain with pictures.
     buildSeparationDisplayImgs(response.link);
-
   }
+
+}
+  //=======================================================
 
         //KEEP B BELOW
 //
@@ -129,17 +114,13 @@ function baconOracleDone(response) {
 // console.log(url);
 //     apiGet(url, doneBaconOracle, failBaconOracle);
 //   }
+//=======================================================
 
-  else if (response.status === "error") {
-    clearResults();
-    console.log("Error retrieving data");
-
-  }
-}
-
+//=======================================================
+// Remove the years at the end of the movie titles, if any.
 function removeYears(artistWorkStringArray) {
   artistWorkStringArray.forEach(function (element, index) {
-    var indexPara = element.indexOf("(");
+    var indexPara = element.indexOf('(');
 console.log("para index: ", element, indexPara);
     if (indexPara === -1) {
       artistWorkStringArray[index]=element.slice(0);
@@ -148,19 +129,12 @@ console.log("doing the para extraction!");
       artistWorkStringArray[index]=element.slice(0, indexPara);
 console.log("Work without para: ", artistWorkStringArray[index]);
     }
-    // var arrayString = element.split('');
-    // var index = arrayString.indexOf("(");
-    // if !index {
-    //   arrayString.splice(index, -1);
-    // }
-    // artistWorkStringArray[index] = (arrayString.join(''));
   });
 }
+//=======================================================
 
-// function spliceSlice(str, index, count, add) {
-//   return str.slice(0, index) + (add || "") + str.slice(index, -11);
-// }
-
+//=======================================================
+// Build the text connection display
 function buildSeparationDisplay(separationArray) {
 console.log("separationArray = ", separationArray);
   //The first element is an artist, and the array
@@ -179,41 +153,16 @@ console.log("separationArray = ", separationArray);
       //If there are more artists to follow,
       // display another chain.
       if ((i+2) < separationArray.length)  {
-        console.log("why am i here? ", "i+2 = ", (i+2), separationArray.length)
         displayNode("|", "chain");                  // chain link
       }
     }
   }
 }
 
-// OLD:
-// function buildSeparationDisplayImgs(separationArray) {
-// console.log("IMAGE separationArray = ", separationArray);
-// for (var i = 0; i < separationArray.length; i+=2) {
-//   console.log("separationArray = ", separationArray);
-//   //Display artist, chain, and work.  First the artist.
-//   //Get the image for the next artist and work.
-//   image = getImage(separationArray[i], "artist")
-//   console.log("image in build = ", image);
-//   displayImgNode(image);
-//
-//   //If this is not the last artist, display the connecting work.
-//   if ((i+2) < separationArray.length) {
-//     // displayImgNode("|", "chain", i);
-//     image = getImage(separationArray[i+1], "movie");         // chain link
-//     console.log("image = ", image);
-//     displayImgNode(image);  // work
-//   }
-//
-//   //If there are more artists to follow,
-//   // display another chain.
-//   if ((i+2) < separationArray.length)  {
-//     //  displayImgNode("|", "chain");                  // chain link
-//   }
-// }
-// }
+//=======================================================
 
-//NEW
+//=======================================================
+// Build the URLs for the image connection display
 function buildSeparationDisplayImgUrls(separationArray) {
   var separationArrayUrls=[];
 console.log("separationArray = ", separationArray);
@@ -231,87 +180,86 @@ console.log("url of movie: ", url);
   };
   return (separationArrayUrls);
 };
+//=======================================================
 
+//=======================================================
+// Build the text connection display
 function buildSeparationDisplayImgs(separationArray) {
   // Take an array of promises and wait on them all
   var separationArrayNameUrls =  buildSeparationDisplayImgUrls(separationArray);
   var separationArrayIdUrls = [];
-console.log("separationArrayUrls = ", separationArrayNameUrls);
+console.log("separationArrayNameUrls = ", separationArrayNameUrls);
   return Promise.all(
-    separationArrayNameUrls.map(apiGetWait).then (
+    separationArrayNameUrls.map(apiGetWait)).then (
       function(idArray) {
-        idArray.map(convertUrlToId);
-        return Promise.all(
-          idArray.map(apiGetWait).then (
-            //call function to draw images
-            function(imgArray)
-            displaySeparationImgs(imgArray);
+        console.log("idArray = ", idArray);
+        var urlArray = idArray.map(convertUrlToId);
+        console.log("urlArray = ", urlArray);
+        return Promise.all (
+          urlArray.map(apiGetWait)).then (
+            function(imgArray) {
+              console.log("imgArray = ", imgArray)
+              displaySeparationImgs(imgArray);
+            }
           )
-        )
       }
     )
-  }
+}
 
+//=======================================================
+// Build one URL for either the movie or person location.
+function convertUrlToId(element, index) {
+  var id = "";
+  console.log("element = ", element);
+  if (element.results.length === 0) {
+    console.log("No image for this element");
+  } else {
+    if ( element.results[0].hasOwnProperty('title') ) {
+      //This is a movie, so get the movie id.
+      id = tmdbUrlBase  + "movie/" + element.results[0].id + "/images" + tmdbUrlKey;
+
+    } else {
+      //This is a person, so get the person id.
+      id = tmdbUrlBase + "person/" + element.results[0].id + "/images" + tmdbUrlKey;
+    }
+    console.log("id = ", id);
+
+    return(id);
+  }
+}
+
+//=======================================================
+
+//=======================================================
+// Iterate through the array of images, and display them
+// with chain nodes in between.
 function displaySeparationImgs(separationArray) {
 console.log("separationArray = ", separationArray);
   // Cycle through each element of the response to add to response display
-  for (var i = 0; i < separationArray.length; i++) {
+  for (var i = 0; i < separationArray.length; i+=2) {
     //Display artist, chain, and work.  First the artist.
+    console.log("calling displayImgNode with artist", i, separationArray[i]);
     var artistFilePath =
-    displayImgNode(separationArray[i], "artist", i);      // artist
+    displayImgNode(separationArray[i], "artist");      // artist
 
     //If this is not the last artist, display the connecting work.
     if ((i+2) <= separationArray.length) {
       displayNode("|", "chain", i);                   // chain link
-      displayImgNode(separationArray[i+1], "work", i);  // work
+      console.log("calling displayImgNode with movie", i+1, separationArray[i+1]);
+      displayImgNode(separationArray[i+1], "movie");  // work
 
       //If there are more artists to follow,
       // display another chain.
       if ((i+2) < separationArray.length)  {
-        console.log("why am i here? ", "i+2 = ", (i+2), separationArray.length)
         displayNode("|", "chain");
       }
     }
   }
 }
 
-function getJSONPromise(url) {
-  return (apiGetWait(url));
-  // return apiGetWait(url).catch(function(error) {     Try later.
-  //   console.log("getJSON failed for", url, error);
-  //   throw err;
-}
+//=======================================================
 
-
-// //Old buildSeparationDisplayImgs
-//   //The first element is an artist, and the array
-//   // alternates between artists and works from there on.
-//   ////////New below
-//   for (var i = 0; i < separationArray.length; i+=2) {
-// console.log("separationArray = ", separationArray);
-//     //Display artist, chain, and work.  First the artist.
-//     //Get the image for the next artist and work.
-//     image = getImageUrl(separationArray[i], "artist")
-// console.log("image in build = ", image);
-//     displayImgNode(image);
-//
-//     //If this is not the last artist, display the connecting work.
-//     if ((i+2) < separationArray.length) {
-//       // displayImgNode("|", "chain", i);
-//       image = getImageUrl(separationArray[i+1], "movie");         // chain link
-//       console.log("image = ", image);
-//       displayImgNode(image);  // work
-//     }
-//
-//     //If there are more artists to follow,
-//     // display another chain.
-//     if ((i+2) < separationArray.length)  {
-//     //  displayImgNode("|", "chain");                  // chain link
-//    }
-//  }
-// }
-////////New above
-
+//=======================================================
 // Build the API URL string depending on the type of image requested.
 function getImgUrl(text, type) {
   var filePath;
@@ -324,80 +272,38 @@ function getImgUrl(text, type) {
   return(filePath);
 }
 
-//     var imageUrl = tmdbUrlImgPerson + data["results"][0].id + "/images" + tmdbUrlKey;
-//       var fullFilePath = tmdbImgUrlBasePerson + data.profiles[0].file_path;
-
-//Old one
-// function getImagePerson(name) {
-//   console.log("getImagePerson api call 1: ", tmdbUrlSearchPerson + name);
-//   var getterId = apiGetWait(tmdbUrlSearchPerson+name);
-// console.log("getterId = ", getterId);
-//   var getterImage = getterId.then(function(data) {
-// console.log("data = ", data);
-//     // var imageUrl = tmdbUrlImgPerson + data["results"][0].id + "/images" + tmdbUrlKey;
-//     var imageUrl = tmdbUrlImgPerson + data["results"][0].id + "/images" + tmdbUrlKey;
-//     console.log("imageUrl:-- ", imageUrl);
-//     return(apiGetWait(imageUrl));
-//   });
-//   console.log("tmdbImgUrlBasePerson = ", tmdbImgUrlBasePerson);
-//
-//     $.when(getterImage).done(function(data) {
-//       var fullFilePath = tmdbImgUrlBasePerson + data.profiles[0].file_path;
-//       console.log("fullFilePath = ", fullFilePath);
-//       return(fullFilePath);
-//     }
-//   )
-//   return("Oops - beat the API for person");
-// }
-//New one
-function getImagePersonUrl(name) {
-  return (tmdbUrlSearchPerson + name);
-}
-//     var imageUrl = tmdbUrlImgMovie + data["results"][0].id + "/images" + tmdbUrlKey;
-//       var fullFilePath = tmdbImgUrlBaseMovie + data.backdrops[0].file_path;
-
-
-// $.when( { testing: 123 } ).done(function( x ) {
-
-//Old one
-// function getImageMovie(title) {
-//   console.log("getImageMovie api call 1: ", tmdbUrlSearchMovie + title);
-//   var getterId = apiGetWait(tmdbUrlSearchMovie+title);
-//
-//   var getterImage = getterId.then(function(data) {
-//     console.log("getterImage data in getImageMovie 1: ", data);
-//     var imageUrl = tmdbUrlImgMovie + data["results"][0].id + "/images" + tmdbUrlKey;
-//     console.log("imageUrl: ", imageUrl);
-//     return(apiGetWait(imageUrl))
-//   });
-//     getterImage.done(function(data) {
-//       console.log("getterImage data in getImageMovie 2: ", data);
-//       var fullFilePath = tmdbImgUrlBaseMovie + data.backdrops[0].file_path;
-//       console.log("fullFilePath = ", fullFilePath);
-//       return(fullFilePath);
-//     }
-//   )
-//   return("Oops - beat the API for movie");
-// }
-
-//New one
+//=======================================================
+// Build the API URL string for a movie
 function getImageMovieUrl(title) {
   return (tmdbUrlSearchMovie + title);
 }
+//=======================================================
 
+//=======================================================
+//Build the API URL string for a person
+function getImagePersonUrl(artist) {
+  return (tmdbUrlSearchPerson + artist);
+}
+
+//=======================================================
+
+//=======================================================
+// Display one plain node of the chain to the results display.
 function displayNode(text, type, arrayIndex) {
-  // var node = $("<p></p>").innerHTML(text);     // Create with jQuery
-  // $(node).html(text);
-  // $(node).attr("id", arrayIndex);
-
   var node = document.createElement("p");
   node.innerHTML = text;
   $(node).prop("class", "chainNode");
   $('#resultsChain').append(node);
 }
 
+//=======================================================
+
+//=======================================================
+// Display one image node of the chain to the results display.
 function displayImgNode(image, type) {
+  console.log("IMAGE = ", image);
   var filePath = imgFilePath(image, type);
+  console.log("FILEPATH = ", filePath);
 
   var node = document.createElement("img");
 console.log("image = ", image);
@@ -408,39 +314,59 @@ console.log($(node).attr("src"));
   $('#resultsImgChain').append(node);
 }
 
+//=======================================================
+// Build the image file path for the image location.
 function imgFilePath(filePath, type) {
+  console.log("FILEPATHHHH = ", filePath, "type = ", type);
+  console.log("type =", type);
   if (type === "artist") {
-    var wholeFilePath = tmdbUrlImgPerson + "profiles[0]" + "." + filePath;
+    var wholeFilePath = tmdbImgUrlPerson + filePath["profiles"][0]["file_path"];
+
   } else {
-    var wholeFilePath = tmdbUrlImgMovie + "backdrops[0]" + "." + filePath;
+    var wholeFilePath = tmdbImgUrlMovie + filePath["backdrops"][0]["file_path"];
   }
 console.log("filePath =" , wholeFilePath);
   return (wholeFilePath);
 }
 
+//=======================================================
+
+//=======================================================
+// Empty out the results panels for both text and
+// image displays.
 function clearResults() {
   $('#resultsChain').empty();
   $('#resultsImgChain').empty();
 }
 
+//=======================================================
+// Warn user upon a Bacon Oradle API fail
 function baconOracleFail(response) {
   console.log("Failed Bacon Oracle");
 }
+//=======================================================
 
+//=======================================================
+// Do some prep to get the console complete.
 function readyStart() {
-  console.log(tmdbUrlConfig);
   apiGet(tmdbUrlConfig, readyDone, readyFail);
 }
+//=======================================================
 
+//=======================================================
+// Do more prep once we get the image locations
+// from the TMDB API.
 function readyDone(response) {
   tmdbImgUrlBase = response["images"]["base_url"];
-  // tmdbImgUrlBase = response.tmdbImgs.base_url;
-  tmdbImgUrlBasePerson = tmdbImgUrlBase  + tmdbImgSizePerson;
-  tmdbImgUrlBaseMovie = tmdbImgUrlBase + tmdbImgSizeMovie;
+  tmdbImgUrlPerson = tmdbImgUrlBase  + tmdbImgSizePerson;
+  tmdbImgUrlMovie = tmdbImgUrlBase + tmdbImgSizeMovie;
 }
 
+//=======================================================
+// Warn the user after a TMDB API call.
 function readyFail(response) {
   console.log("Failed Ready Start");
 }
+//=======================================================
 
 $(document).ready(readyStart());
